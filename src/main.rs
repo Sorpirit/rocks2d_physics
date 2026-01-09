0605a646c5ac35e9f63f4de557955eeda2d42f38
Sorpirit <danvuk11@gmail.com>
2026-01-08 21:20:13 -0500

    Add rigid body properties

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,85 @@
 use raylib::prelude::*;
 use raylib::math::{Vector2};
 
+#[derive(Debug, Copy, Clone)]
 pub enum Shape {
     Circle(f32), //radius
     Rectangle(f32, f32), //half w, h
 }
 
-pub struct Entity {
+#[derive(Debug, Copy, Clone)]
+pub struct Transform2D {
     pub position: Vector2,
     pub rotation: f32,
+}
+
+impl Transform2D {
+    fn zero() -> Transform2D {
+        Transform2D { position: Vector2::zero(), rotation: 0.0 }
+    }
+}
+
+pub struct Entity {
+    pub transform: Transform2D,
+    pub prev_transform: Transform2D,
+
+    pub velocity: Vector2,
+    pub angular_velocity: f32,
+
+    pub mass: f32,
+    pub inertia: f32,
+
+    pub inv_mass: f32,
+    pub inv_inertia: f32,
 
     pub shape: Shape,
     pub color: Color
+}
+
+
+fn calcualte_area(shape: Shape) -> f32 {
+    match shape {
+        Shape::Circle(r) => PI as f32 * r * r,
+        Shape::Rectangle(wh, hh) => 4.0 * wh * hh
+    }
+}
+
+fn calculate_mass(shape: Shape, density: f32) -> f32 {
+    calcualte_area(shape) * density
+}
+
+fn calculate_inertia(shape: Shape, mass: f32) -> f32 {
+    match shape {
+        Shape::Circle(r) => mass * r * r / 4.0,
+        Shape::Rectangle(wh, hh) => mass * (wh * wh + hh * hh) / 4.0
+    }
+}
+
+
+impl Entity {
+    fn new(transform: Transform2D, shape: Shape, density: f32) -> Entity {
+        let mut ent = Entity { 
+            transform: transform,
+            prev_transform: transform, 
+            velocity: Vector2::zero(), 
+            angular_velocity: 0.0, 
+            mass: calculate_mass(shape, density), 
+            inertia: 0.0, 
+            inv_mass: 0.0, 
+            inv_inertia: 0.0, 
+            shape: shape, 
+            color: Color::DARKSEAGREEN 
+        };
+
+        if ent.mass > 0.0 {
+            ent.inertia = calculate_inertia(shape, ent.mass);
+            
+            ent.inv_mass = 1.0 / ent.mass;
+            ent.inv_inertia = 1.0 / ent.inertia;
+        }
+
+        return ent;
+    }
 }
 
 fn main() {
@@ -36,10 +104,9 @@ fn main() {
     }; 
 
     let mut entities = vec![
-        Entity { position: Vector2::new(2.0, 0.0), rotation: 0.0, shape: Shape::Circle(0.5), color: Color::BLUE },
-        Entity { position: Vector2::new(-1.0, 1.0), rotation: 0.0, shape: Shape::Circle(1.0), color: Color::RED },
-        Entity { position: Vector2::new(0.5, 0.0), rotation: 0.0, shape: Shape::Rectangle(1.0, 0.5), color: Color::GREEN }
-
+        Entity::new(Transform2D { position: Vector2::new(2.0, 0.0), rotation: 0.0 }, Shape::Circle(0.5), 1000.0),
+        Entity::new(Transform2D { position: Vector2::new(-1.0, 1.0), rotation: 0.0 }, Shape::Circle(1.0), 1000.0),
+        Entity::new(Transform2D { position: Vector2::new(0.5, 0.0), rotation: 0.0 }, Shape::Rectangle(1.0, 0.5), 1000.0),
     ];
 
     let mut total_time = 0.0;
@@ -59,12 +126,12 @@ fn main() {
 
         // todo physics sim
         for ent in &mut entities {
-            ent.position.y = (total_time).sin();
-            ent.rotation = (total_time).cos() * 1.0 * 3.14;
+            ent.transform.position.y = (total_time).sin();
+            ent.transform.rotation = (total_time).cos() * 1.0 * 3.14;
         }
 
         let target_ent = &mut entities[0];
-        target_ent.position = (mouse_pos + camera.target - camera.offset) / world_scalar;
+        target_ent.transform.position = (mouse_pos + camera.target - camera.offset) / world_scalar;
 
         //rendering
         let mut d = rl.begin_drawing(&thread);
@@ -80,18 +147,18 @@ fn main() {
 
         let mut r2d = d.begin_mode2D(camera);
         for ent in &entities {
-            let view_pos = ent.position * world_scalar;
+            let view_pos = ent.transform.position * world_scalar;
             match ent.shape {
                 Shape::Circle(r) => {
                     r2d.draw_circle_v(view_pos, r * world_scalar.x, ent.color);
-                    r2d.draw_line_v(view_pos, view_pos + Vector2::new(r * world_scalar.x, 0.0).rotated(ent.rotation), Color::BLACK);
+                    r2d.draw_line_v(view_pos, view_pos + Vector2::new(r * world_scalar.x, 0.0).rotated(ent.transform.rotation), Color::BLACK);
                 },
                 Shape::Rectangle(width_half, height_half) => {
                     let wh_view = width_half * world_scalar.x;
                     let hh_view = height_half * world_scalar.x;
-                    r2d.draw_rectangle_pro(Rectangle { x:  view_pos.x, y: view_pos.y, width: wh_view * 2.0, height: hh_view * 2.0 }, Vector2::new(wh_view, hh_view), ent.rotation.to_degrees(), ent.color);
+                    r2d.draw_rectangle_pro(Rectangle { x:  view_pos.x, y: view_pos.y, width: wh_view * 2.0, height: hh_view * 2.0 }, Vector2::new(wh_view, hh_view), ent.transform.rotation.to_degrees(), ent.color);
                     r2d.draw_circle_v(view_pos, 10.0, Color::YELLOW);
-                    r2d.draw_line_v(view_pos, view_pos + Vector2::new(wh_view, 0.0).rotated(ent.rotation), Color::BLACK);
+                    r2d.draw_line_v(view_pos, view_pos + Vector2::new(wh_view, 0.0).rotated(ent.transform.rotation), Color::BLACK);
                 }
             }
         }
